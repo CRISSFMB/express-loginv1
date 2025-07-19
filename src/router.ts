7d3c2b87f22bdf7eb805fb5c12eaaa8f0f58e4b8
cristian mosquera <cmosqbone1@gmail.com>
2025-07-20 00:42:41 +0200

    creando el modelo usuario los schemas y ingresando los datos a la base de datos y enviando la respuesta de que el usuario se creo correctamente

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import User from './models/User';
 const router = express.Router();
 
-router.post('/auth/register', (req, res) => {
-    res.status(201).json({ message: 'Usuario recibido', data: req.body });
+router.post('/auth/register', async (req, res) => {
+    try {
+        await User.create(req.body);
+
+        res.status(202).json({
+            message: 'User registered successfully',
+            user: req.body.name
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 export default router;
