f297349e65e5b35150c29ed5bb94a2b44684382b
cristian mosquera <cmosqbone1@gmail.com>
2025-07-21 22:09:48 +0200

    agregando hash a los passwords y guardando en la base de datos

--- src/controlers/index.ts
+++ src/controlers/index.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 
 export const createUser = async (req: Request, res: Response) => {
 
+    // recibimos el email y la contraseña del body de la peticion
+    const { email, password } = req.body;
+
+    // usamos findOne para buscar si el usuario ya existe en la base de datos
+    const userExist = await User.findOne({ email })
+
+
+    // validamos el email 
+    if (userExist) {
+        const error = new Error('El email ya existe');
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+
+    const scryptPassword = await hashPassword(password)
+    // hasheamos la contraseña
+
     try {
-        await User.create(req.body);
+        const user = new User(req.body);
+
+        user.password = scryptPassword;
+        // guardamos el usuario en la base de datos
+        await user.save();
 
         res.status(201).json({
             message: 'User registered successfully'
